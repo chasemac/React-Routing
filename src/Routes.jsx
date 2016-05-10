752d3f289ab27220808175ff7ed48732dc73c524
Chase McElroy <ichasem@gmail.com>
2016-05-09 18:33:13 -0700

    Fixed more typos and got it to compile

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,4 +1,5 @@
-var React = require('react') : var ReactRouter = require('react-router');
+var React = require('react');
+var ReactRouter = require('react-router');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 
@@ -9,8 +10,8 @@ var Page2 = require('./components/Page2.jsx');
 var Routes = (
     <Router>
         <Route path="/" component={Base}>
-            <Route path="/page1" component={Page1}/>
-            <Route path="/page2" component={Page2}/>
+            <Route path="/page1" component={Page1} />
+            <Route path="/page2" component={Page2} />
         </Route>
     </Router>
 );
